158780598b42493746003b5f7d00d9742284ea89
mongkok <domake.io@gmail.com>
2018-01-15 04:09:00 +0530

    Remove assert_called

--- tests/mixins.py
+++ tests/mixins.py
@@ -15,27 +15,24 @@ def social_auth_mock(f):
 class SocialAuthTestsMixin(object):
 
     @social_auth_mock
-    def test_social_auth(self, login_mock, *args):
+    def test_social_auth(self, *args):
         response = self.execute({
             'provider': 'google-oauth2',
             'accessToken': '-token-',
         })
 
         social = response.data['socialAuth']['social']
-
-        login_mock.assert_called_once()
         self.assertEqual('test', social['uid'])
 
 
 class SocialAuthJWTTestsMixin(object):
     @social_auth_mock
     @patch.dict(sys.modules, {'graphql_jwt.shortcuts': None})
-    def test_social_auth_import_error(self, login_mock, *args):
+    def test_social_auth_import_error(self, *args):
         response = self.execute({
             'provider': 'google-oauth2',
             'accessToken': '-token-',
         })
 
-        login_mock.assert_called_once()
         self.assertTrue(response.errors)
         self.assertIsNone(response.data['socialAuth'])
