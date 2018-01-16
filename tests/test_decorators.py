102ec4738f02e3a5275c7b09381be854066f7cad
mongkok <domake.io@gmail.com>
2018-01-16 23:10:57 +0530

    100% coverage

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,8 +1,10 @@
 from unittest.mock import MagicMock
 
-from django.test import TestCase
+from django.test import TestCase, override_settings
 
-from graphql_social import decorators, exceptions
+from graphql_social_auth import decorators, exceptions
+
+from .decorators import social_auth_mock
 
 
 class DecoratorsTests(TestCase):
@@ -13,6 +15,19 @@ class DecoratorsTests(TestCase):
         def wrapped(self, root, info, provider, *args, **kwargs):
             """Social auth decorated function"""
 
+        mock = MagicMock()
+
         with self.assertRaises(exceptions.GraphQLSocialError):
-            mock = MagicMock()
             wrapped(self, mock, mock, 'unknown', '-token-')
+
+    def test_do_auth_user_not_found(self):
+        @social_auth_mock
+        @override_settings(SOCIAL_AUTH_PIPELINE=[])
+        @decorators.social_auth
+        def wrapped(self, root, info, provider, *args, **kwargs):
+            """Social auth decorated function"""
+
+        mock = MagicMock()
+
+        with self.assertRaises(exceptions.GraphQLSocialError):
+            wrapped(self, mock, mock, 'google-oauth2', '-token-')
