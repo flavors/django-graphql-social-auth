3b583f5eee065c2dd24bb410dc5ca912a520916e
mongkok <domake.io@gmail.com>
2018-02-12 23:57:41 +0530

    Removed pipelines

--- graphql_social_auth/pipelines.py
+++
@@ -1,5 +0,0 @@
-
-def associate_signed_user(strategy, social=None, *args, **kwargs):
-    user = strategy.request.user
-    if user.is_authenticated and social is None:
-        return {'user': user}
