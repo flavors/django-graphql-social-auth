ef87e6728248703ad146257b95b06bfe84c46caa
mongkok <domake.io@gmail.com>
2019-02-07 20:55:46 +0700

    Replaced django login with social _do_login

--- graphql_social_auth/decorators.py
+++ graphql_social_auth/decorators.py
@@ -1,50 +1,40 @@
 from functools import wraps
 
-from django.contrib.auth import login
 from django.utils.translation import ugettext_lazy as _
 
 from promise import Promise, is_thenable
 from social_core.exceptions import MissingBackend
 from social_django.utils import load_backend, load_strategy
+from social_django.views import _do_login
 
 from . import exceptions, mixins
-
-__all__ = ['psa', 'social_auth']
-
-
-def get_user(context, provider, access_token):
-    strategy = load_strategy(context)
-
-    try:
-        backend = load_backend(strategy, provider, redirect_uri=None)
-    except MissingBackend:
-        raise exceptions.GraphQLSocialAuthError(_('Provider not found'))
-
-    user = backend.do_auth(access_token)
-
-    if user is None:
-        raise exceptions.InvalidTokenError(_('Invalid token'))
-
-    user_model = strategy.storage.user.user_model()
-
-    if not isinstance(user, user_model):
-        msg = _('`{}` is not a user instance').format(type(user).__name__)
-        raise exceptions.DoAuthError(msg, user)
-
-    return user
 
 
 def psa(f):
     @wraps(f)
     def wrapper(cls, root, info, provider, access_token, **kwargs):
-        user = get_user(info.context, provider, access_token)
+        strategy = load_strategy(info.context)
+
+        try:
+            backend = load_backend(strategy, provider, redirect_uri=None)
+        except MissingBackend:
+            raise exceptions.GraphQLSocialAuthError(_('Provider not found'))
+
+        user = backend.do_auth(access_token)
+
+        if user is None:
+            raise exceptions.InvalidTokenError(_('Invalid token'))
+
+        user_model = strategy.storage.user.user_model()
+
+        if not isinstance(user, user_model):
+            msg = _('`{}` is not a user instance').format(type(user).__name__)
+            raise exceptions.DoAuthError(msg, user)
 
         if not issubclass(cls, mixins.JSONWebTokenMixin):
-            login(info.context, user)
+            _do_login(backend, user, user.social_user)
 
-        social = user.social_auth.get(provider=provider)
-
-        return f(cls, root, info, social, **kwargs)
+        return f(cls, root, info, user.social_user, **kwargs)
     return wrapper
 
 
